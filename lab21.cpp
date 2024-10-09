4dd31e994b7a3fb5604b5da21c92eaeba373ebd2
AudreyDike <adts222@gmail.com>
2024-10-09 15:23:38 -0700

    Updated lab21.cpp

--- lab21.cpp
+++ lab21.cpp
@@ -13,3 +13,30 @@ public:
     int age;
     string name;
     string color;
+  // Default constructor with random values
+    Goat() {
+        age = rand() % (MAX_AGE - MIN_AGE + 1) + MIN_AGE;
+        name = names[rand() % NAME_COUNT];
+        color = colors[rand() % COLOR_COUNT];
+    }
+
+    // Parameterized constructor
+    Goat(int a, string n, string c) : age(a), name(n), color(c) {}
+};
+class DoublyLinkedList {
+private:
+    struct Node {
+        Goat data;
+        Node* prev;
+        Node* next;
+        Node(Goat val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() { head = nullptr; tail = nullptr; }
